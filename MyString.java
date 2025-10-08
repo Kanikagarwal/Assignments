5fa5ae6a06935b29c118350b66f7df0782743328
Kanika Agarwal <kanikaweb26@gmail.com>
2025-10-09 00:05:09 +0530

    third commit

--- MyString.java
+++ MyString.java
@@ -19,95 +19,107 @@ public class MyString {
     }
 
     int countWords(){
-        if(value.length()==0)return 0;
+        if(value.length() == 0)return 0;
         boolean word = false;
-        int count=0;
-        for(int i=0;i<value.length();i++){
-            word=(value.charAt(i)!=' ');
-            if(word && (i==0 || value.charAt(i-1)==' '))count++;
+        int count = 0;
+        for(int i = 0; i < value.length(); i++){
+            word=(value.charAt(i) != ' ');
+            if(word && (i == 0 || value.charAt(i-1) == ' '))count++;
         }
         return count;
     }
 
-    String replace(char a,char b){
-        char[] arr = value.toCharArray();
-        for(int i=0;i<value.length();i++){
-            if(arr[i]==a){
-                arr[i]=b;
+    String replace(char toReplace,char replaceBy){
+        char[] array = value.toCharArray();
+        for(int i = 0; i < value.length(); i++){
+            if(array[i] == toReplace){
+                array[i] = replaceBy;
             }
         }
-        this.value=new String(arr);
+        this.value = new String(array);
         return this.value;
     }
 
     boolean isPalindrome(){
-        int i=0;
-        int j=value.length()-1;
-        while(i<j){
-            if(value.charAt(i)!=value.charAt(j))return false;
+        int i = 0;
+        int j = value.length()-1;
+        while(i < j){
+            if(value.charAt(i) != value.charAt(j))return false;
             i++;
             j--;
         }
         return true;
     }
 
-    String[] split(){
+    String[] getSplit(){
         int n = countWords();
-        String[] arr = new String[n];
-        int i=0;
-        int index=0;
-        while(i<value.length()){
-            while (i<value.length() && value.charAt(i)==' ')i++;
-            if(i>=value.length())break;
+        String[] array = new String[n];
+        int i = 0;
+        int index = 0;
+        while(i < value.length()){
+            while(i < value.length() && value.charAt(i) == ' ')i++;
+            if(i >= value.length())break;
             int j=i;
-            while(j<value.length() && value.charAt(j)!=' ')j++;
-            arr[index++]=value.substring(i,j);
-            i=j+1;
+            while(j < value.length() && value.charAt(j) != ' ')j++;
+            array[index++] = value.substring(i,j);
+            i = j+1;
         }
 
-        return arr;
+        return array;
     }
 
     String splice(int start,int length){
-        if(start<0 || start>value.length() || length<=0)return value;
-        int end=Math.min(start+length,value.length());
-        this.value=value.substring(0,start)+value.substring(end);
+        if(start < 0 || start > value.length() || length <= 0)return value;
+        int end = start+length;
+        if(end > value.length())end = value.length();
+        this.value = value.substring(0,start) + value.substring(end);
         return this.value;
     }
 
-    char maxRepeat(){
+    char getMaxRepeat(){
         if(value.length() == 0) return '\0';
-        int arr[] = new int[256];
-        int max=0;
-        char character=value.charAt(0);
-        for(int i=0;i<value.length();i++){
-            char c = value.charAt(i);
-            arr[c]++;
-            if(arr[c]>max){
-                max=arr[c];
-                character=c;
+        int array[] = new int[256];
+        int maxCount = 0;
+        char maxCharacter = value.charAt(0);
+        for(int i = 0;i < value.length(); i++){
+            char character = value.charAt(i);
+            array[character]++;
+            if(array[character] > maxCount){
+                maxCount =  array[character];
+                maxCharacter = character;
             }
         }
-        return character;
+        return maxCharacter;
     }
 
-    String sort(){
-        char a[] = value.toCharArray();
-        Arrays.sort(a);
-        return this.value = new String(a);
+    String sortString() {
+        char array[] = value.toCharArray();
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    char temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        this.value = new String(array);
+        return this.value;
     }
 
     //        9.Shift
-    String shift(int n){
+    String getShift(int index){
         if (value.length() == 0) return value;
 //            Left shift
-        n=n%value.length();
-        String s1=value.substring(n);
-        String s2=value.substring(0,n);
-        return this.value = s1+s2;
+        index = index % value.length();
+        String string1 = value.substring(index);
+        String string2 = value.substring(0,index);
+        return this.value = string1 + string2;
     }
 
-    public String reverse(){
+    public String getReverse(){
         String value = this.value;
         this.value = "";
         for (int i = value.length() - 1; i >= 0; i--){
